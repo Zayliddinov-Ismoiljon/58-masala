d216a4862d0990659057ce15be31c657363a1a7f
Zayliddinov-Ismoiljon <zayliddinovismoiljon@gmail.com>
2022-03-23 18:00:31 +0500

    Innitial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// Masala: n soni berilgan 2 soning dastlabki n ta darajasidan tashkil topgan massiv hosil qiling
+
+let n=5;
+const a=[];
+
+for(i=0; i<=n; i++){
+    let b=2**i;
+    console.log(a.push(b));
+}
